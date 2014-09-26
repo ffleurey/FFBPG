3fdf311fd4953b491e943bb40cab56c183467461
Franck Fleurey <franck@fleurey.com>
2014-09-26 15:57:29 +0200

    added corresponding test

--- src/test/java/eu/diversify/ffbpg/FacadeTest.java
+++ src/test/java/eu/diversify/ffbpg/FacadeTest.java
@@ -1,5 +1,7 @@
 package eu.diversify.ffbpg;
 
+import eu.diversify.ffbpg.random.IntegerGenerator;
+import eu.diversify.ffbpg.random.IntegerSetGenerator;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -35,6 +37,26 @@ public class FacadeTest
      
         assertEquals(g.getServices().size(), 80);
         assertEquals(g.getApplications().size(), 100);
+        assertEquals(g.getPlatforms().size(), 100);
+    }
+    
+    public void testGenerateBPGraphRandom()
+    {
+        int number_platforms = 100;
+        int number_services = 75;
+        int number_applications = 150;
+        
+        int platforms_capacity = 10;
+        int application_capacity = number_services; // No Limit
+        
+        // Random distribution generators used both for apps and plats
+        IntegerGenerator sizes_generator = Facade.getPoissonIntegerGenerator(6); 
+        IntegerSetGenerator srv_generator = Facade.getNegExpIntegerSetGenerator(0.25, 0.005);
+        
+        
+        BPGraph g = Facade.createRandomBPGraph(number_applications, number_platforms, number_services,
+                    sizes_generator, srv_generator, application_capacity, platforms_capacity);
+     
         assertEquals(g.getPlatforms().size(), 100);
     }
     
